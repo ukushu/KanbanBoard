7d8d3550fe0d0201b908e8c8d99c90b37a1256f4
UKS <skulptorrr@duck.com>
2025-07-31 23:30:48 +0300

    WIP

--- KanbanBoard/Scenes/ProjView.swift
+++ KanbanBoard/Scenes/ProjView.swift
@@ -6,24 +6,26 @@ struct ProjView: View {
     let projID: ProjID
     @ObservedObject var boardsListDocument : Flow.Document<[UUID]>
     
-    let kBoardID: KBoardID
-    
     init(projID: ProjID) {
         self.projID = projID
         self.boardsListDocument = projID.boardsListDocument
-        
-        if let uuidBoardId = projID.boardsListDocument.content.first {
-            self.kBoardID = KBoardID(projID: projID, uuid: uuidBoardId)
-        } else {
-            let kBoardID = KBoardID(projID: projID)
-            projID.boardsListDocument.content = [kBoardID.id]
-            self.kBoardID = kBoardID
-        }
     }
     
     var body: some View {
         ZStack {
-            KBoardView(kBoardID: kBoardID)
+            KBoardView(kBoardID: projID.defaultBoardID)
+        }
+    }
+}
+
+extension ProjID {
+    var defaultBoardID : KBoardID {
+        if let uuidBoardId = self.boardsListDocument.content.first {
+            return KBoardID(projID: self, uuid: uuidBoardId)
+        } else {
+            let kBoardID = KBoardID(projID: self)
+            self.boardsListDocument.content = [kBoardID.id]
+            return kBoardID
         }
     }
 }
